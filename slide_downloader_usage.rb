3d5ecd15061c707165ee40aae07d1ef9278e2ab5
Taha Yasir Kıroğlu <yasir.kiroglu@bil.omu.edu.tr>
2018-03-23 16:13:22 +0300

    Update slide_downloader_usage.rb

--- slide_downloader_usage.rb
+++ slide_downloader_usage.rb
@@ -1,20 +1,21 @@
 #!/usr/bin/ruby
 load 'slide_share_kit.rb'
 
-url_hsh_example = {
+url_hsh = {
   'nmap' => 'https://www.slideshare.net/bgasecurity/nmap-kullanm-kitap-27487242',
   'yapay zeka' => 'https://www.slideshare.net/SELENGCN/yapay-zek-ve-duygusal-zek-kullanim-farkliliklarinin-ncelenmes-teknolojk-kabul-dzey-eksennde-br-aratirma'
 }
 
 ss = SlideShareDownloader.new
 
+strt = Time.new
 # download single file
 # url_hsh.each do |file_name, link|
 #   ss.download link, file_name, 'full', false
 # end
 
-
 # download paralel
 ss.parallel_download url_hsh, 'full'
+fin = Time.new
 
-
+puts "#{fin -strt} sn"
